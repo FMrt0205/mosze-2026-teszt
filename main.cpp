e5534095e2bf364419b9a2dc4b8f45d0eae164ad
FMrt0205 <martinordasfarkas2004@gmail.com>
2026-02-24 13:51:57 +0100

    hibák kijavítása

--- main.cpp
+++ main.cpp
@@ -5,26 +5,27 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     //helytelen név
-    int *b = new int[NELEMENTS];
-//cout végéről egy ; hiányzik
-    std::cout << '1-100 ertekek duplazasa'
+    int *b = new int[N_ELEMENTS];
+    //cout végéről egy ; hiányzik
+    std::cout << "1-100 ertekek duplazasa";
     //ez egy nagyon fura for loop (hiányzik az iteráció, illetve az összehasonlítás (sztm))
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    //Összehasonlítás
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        // cout végéről egy ; hiányzik
-        std::cout << "Ertek:"
+
+        std::cout << "Ertek:";
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag = 0;
     //itt egy , van a pontos ; helyett
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         // ; végéről hiányzik
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
