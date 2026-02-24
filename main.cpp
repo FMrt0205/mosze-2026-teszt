4478cd374c1d2a4d6062410b3c709eb66d692d9c
FMrt0205 <martinordasfarkas2004@gmail.com>
2026-02-24 13:40:28 +0100

    Hiba kommentek létrehozva

--- main.cpp
+++ main.cpp
@@ -4,20 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    //helytelen név
     int *b = new int[NELEMENTS];
+//cout végéről egy ; hiányzik
     std::cout << '1-100 ertekek duplazasa'
+    //ez egy nagyon fura for loop (hiányzik az iteráció, illetve az összehasonlítás (sztm))
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
+        // cout végéről egy ; hiányzik
         std::cout << "Ertek:"
     }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
+    //itt egy , van a pontos ; helyett
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        // ; végéről hiányzik
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
